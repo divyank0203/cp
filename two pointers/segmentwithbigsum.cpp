36e38541b4d7a4c689b0023b096ac3cfc9b3f8f4
divyank0203 <divyankh444@gmail.com>
2025-07-06 23:19:33 +0530

    initiated segmentwithbigsum

---
+++ two pointers/segmentwithbigsum.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    long long int s;
+    cin>>n>>s;
+    vector<long long int> a(n);
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+
+    }
+
+}
