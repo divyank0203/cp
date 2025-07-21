0b86c573247dc4945589533514a26ec42dfc7603
divyank0203 <divyankh444@gmail.com>
2025-07-21 18:22:28 +0530

    fair

---
+++ maths/faircandydistribution.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    long long k;
+    cin>>n>>k;
+    vector<int> cit(n);
+    for(int i=0; i<n; i++){
+        cin>>cit[i];
+    }
+    vector<long long> ans(n);
+    for(int i=0; i<n; i++){
+        if()
+    }
+
+}
