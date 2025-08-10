776c05f83af3baa2bb55efdbeb88967a5bb58aa7
divyank0203 <divyankh444@gmail.com>
2025-08-11 00:35:05 +0530

    alternating series

--- contest/10 august/alter.cpp
+++ contest/10 august/alter.cpp
@@ -10,19 +10,28 @@ int main(){
         if(n==2){
             cout<<-1<<" "<<2<<endl;
         }
-        else{
+        else if(n%2==0&&n>2){
             for(ll i=0; i<n; i++){
                 if(i%2==0&&i!=(n-1)){
                     cout<<-1<<" ";
                 }
-                else if(i%2==0&&i==(n-1)){
-                    cout<<-1;
-                }
                 else if(i%2!=0&&i!=(n-1)){
                     cout<<3<<" ";
                 }
+                else if(i==(n-1)){
+                    cout<<2<<" ";
+                }
+            }
+
+            cout<<endl;
+        }
+        else if(n%2!=0&&n>2){
+            for(ll i=0; i<n; i++){
+                if(i%2==0){
+                    cout<<-1<<" ";
+                }
                 else{
-                    cout<<3;
+                    cout<<3<<" ";
                 }
             }
             cout<<endl;
