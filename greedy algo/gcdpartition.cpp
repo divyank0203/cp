6f1daf2c858db7b38d204bd8eb225764c4380cba
divyank0203 <divyankh444@gmail.com>
2025-08-31 23:36:49 +0530

    gcd partition

---
+++ greedy algo/gcdpartition.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll= long long;
+bool isprime(ll m){
+    for(ll i=2; i*i<=m; i++){
+        if(m%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        vector<ll> a(n);
+        for(ll i=0; i<n; i++){
+            cin>>a[i];
+        }
+        ll sum = accumulate(a.begin(), a.end(), 0);
+        if(isprime(sum)){
+            cout<<1<<endl;
+        }
+        else{
+            
+        }
+        
+    }
+}
