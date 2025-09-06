49187489d2a8a27a16d966bfb37b3f09cc11fa59
divyank0203 <divyankh444@gmail.com>
2025-09-06 20:55:52 +0530

    binary

--- greedy algo/binaryinversion.cpp
+++ greedy algo/binaryinversion.cpp
@@ -16,47 +16,85 @@ int main(){
             cout<<0<<endl;
         }
         else{
-            if(a[0]!=1&&a[n-1]==0){
-                a[0]=1;
 
-            }
-            else if(a[0]==1&&a[n-1]!=0){
-                a[n-1]=0;
+
+            
+bool flag=false;
+            if(a[0]==0){
+                a[0]=1;
+                flag=true;
             }
             
-            
-            else if(a[0]!=1&&a[n-1]!=0){
-                a[0]=1;
-            }
-            else if(a[0]==1&&a[n-1]==0){
+            unordered_map<ll, ll> mp1;
             for(ll i=0; i<n; i++){
-                if(a[i]==0&&i<n/2){
-                    a[i]=1;
-                    break;
-                }
-
+                mp1[a[i]]++;
             }
-            }
-
-            unordered_map<ll, ll> mp;
-            for(ll i=0; i<n; i++){
-                mp[a[i]]++;
-            }
-
-        ll c=mp[1];
-        ll ans=0;
+        ll c1=mp1[1];
+        ll ans1=0;
         for(ll i=n-1; i>=0; i--){
             if(a[i]==0){
-                ans+=c;
+                ans1+=c1;
 
             }else{
-                c--;
-                if(c==0){
+                c1--;
+                if(c1==0){
                     break;
                 }
             }
         }
-        cout<<ans<<endl;
+        if(flag){
+        a[0]=0;
         }
+
+        bool flag2=false;
+            if(a[n-1]==1){
+                a[n-1]=0;
+                flag2=true;
+            }
+            
+
+            unordered_map<ll, ll> mp2;
+            for(ll i=0; i<n; i++){
+                mp2[a[i]]++;
+            }
+        ll c2=mp2[1];
+        ll ans2=0;
+        for(ll i=n-1; i>=0; i--){
+            if(a[i]==0){
+                ans2+=c2;
+
+            }else{
+                c2--;
+                if(c2==0){
+                    break;
+                }
+            }
+        }
+        if(flag2){
+        a[n-1]=1;
+        }
+
+            unordered_map<ll, ll> mp3;
+            for(ll i=0; i<n; i++){
+                mp3[a[i]]++;
+            }
+        ll c3=mp3[1];
+        ll ans3=0;
+        for(ll i=n-1; i>=0; i--){
+            if(a[i]==0){
+                ans3+=c3;
+
+            }else{
+                c3--;
+                if(c3==0){
+                    break;
+                }
+            }
+        }
+        cout<<max({ans1, ans2, ans3})<<endl;
+
+
+    }
+
     }
 }
