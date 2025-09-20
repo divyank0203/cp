08bd12bdd7b0914764bfaf171e8126005c684c98
divyank0203 <divyankh444@gmail.com>
2025-09-20 16:17:26 +0530

    contest

--- contest/17 sep cc/B.cpp
+++ contest/17 sep cc/B.cpp
@@ -5,6 +5,13 @@ int main(){
     ll t;
     cin>>t;
     while(t--){
-        
+        ll n, k;
+        cin>>n>>k;
+        ll h, m;
+        ll to;
+        to=n*k;
+        h=to/60;
+        m=to%60;
+        cout<<h<<" "<<m<<endl;
     }
 }
--- contest/20 sep cf/C.cpp
+++ contest/20 sep cf/C.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 using ll = long long;
 int main(){
-    ll a, b;
-    cin>>a>>b;
-    ll c=180-(a+b);
-    cout<<c;
+    ll t;
+    cin>>t;
+    while(t--){
+        
+    }
 }
