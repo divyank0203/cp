32029859209583ddb457aa831a6e50c4b39b6fb4
divyank0203 <divyankh444@gmail.com>
2025-09-21 21:42:33 +0530

    contest

--- contest/21 sep cf/A.cpp
+++ contest/21 sep cf/A.cpp
@@ -5,6 +5,30 @@ int main(){
     ll t;
     cin>>t;
     while(t--){
+        ll n;
+        cin>>n; 
+        vector<ll> a(n);
+        for(ll i=0; i<n; i++){
+            cin>>a[i];
+        }
+        unordered_map<ll, ll> mp;
+        for(ll i=0; i<n ; i++){
+            mp[a[i]]++;
+        }
+        vector<pair<ll, ll>> s ;
+        ll siz=mp.size();
         
+        for(auto it : mp){
+            s.push_back({it.second, it.first});
+        }
+        sort(s.begin(), s.end());
+        ll ans=0;
+        ll sum=0;
+        for(ll i=0; i<s.size(); i++){
+            
+            ans=max({ans, (s[i].first)*siz, s[i].first });
+            siz--;
+        }
+        cout<<ans<<endl;
     }
 }
--- contest/21 sep cf/B.cpp
+++ contest/21 sep cf/B.cpp
@@ -5,6 +5,111 @@ int main(){
     ll t;
     cin>>t;
     while(t--){
+        ll n, m;
+        cin>>n>>m;
+        vector<vector<ll>> a(n, vector<ll>(m));
+        unordered_map<ll, ll> mp;
+        for(ll i=0; i<n; i++){
+         ll re;
+         cin>>re;
+         a[i].resize(re);
+            for(ll j=0; j<re; j++){
+                cin>>a[i][j];
+                mp[a[i][j]]++;
+            }
+        }
+        ll q=0;
+        for(ll i=1; i<=m; i++){
+            if(mp[i]==0){
+                q=1;
+                // cout<<"NO"<<endl;
+                break;
+            }
+        }
+        // cout<<" size "<<mp.size()<<endl;
+        if(mp.size()<m||q==1){
+            cout<<"NO"<<endl;
+            continue;
+        }
         
+        
+        ll ns = 0;
+        for (ll i=0; i<n; i++) {
+            bool n = false;
+            for (ll x : a[i]) {
+                if (mp[x]==1) {
+                    n=true;
+                    break;
+                }
+            }
+            if (n==false) {
+                ns++;
+            }
+        }
+        if (ns>=2) {
+            cout <<"YES"<<endl;
+        } else {
+            cout <<"NO"<<endl;
+        }
+            
+        
+        
+
     }
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(0);
+//     int t;
+//     cin >> t;
+//     while (t--) {
+//         int n, m;
+//         cin >> n >> m;
+//         vector<vector<int>> sets(n);
+//         vector<int> cnt(m+1, 0);
+//         for (int i = 0; i < n; i++) {
+//             int l;
+//             cin >> l;
+//             sets[i].resize(l);
+//             for (int j = 0; j < l; j++) {
+//                 cin >> sets[i][j];
+//                 cnt[sets[i][j]]++;
+//             }
+//         }
+//         bool covered = true;
+//         for (int i = 1; i <= m; i++) {
+//             if (cnt[i] == 0) {
+//                 covered = false;
+//                 break;
+//             }
+//         }
+//         if (!covered) {
+//             cout << "NO\n";
+//             continue;
+//         }
+//         int ns = 0;
+//         for (int i = 0; i < n; i++) {
+//             bool n = false;
+//             for (int x : sets[i]) {
+//                 if (cnt[x] == 1) {
+//                     n = true;
+//                     break;
+//                 }
+//             }
+//             if (!n) {
+//                 ns++;
+//             }
+//         }
+//         if (ns >= 2) {
+//             cout << "YES\n";
+//         } else {
+//             cout << "NO\n";
+//         }
+//     }
+//     return 0;
+// }
+
