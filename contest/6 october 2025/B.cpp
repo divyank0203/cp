786913cdb3581f2b9cad97ea7516f03a987b6949
divyank0203 <divyankh444@gmail.com>
2025-10-06 19:54:37 +0530

    contest

---
+++ contest/6 october 2025/B.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using vll = vector<ll> ;
+using vvll = vector<vll> ;
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        
+    }
+}
