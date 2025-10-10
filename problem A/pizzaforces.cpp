6b2696ae72843f992615f46154d10b1997e88b54
divyank0203 <divyankh444@gmail.com>
2025-10-10 21:24:56 +0530

    pizzaforces

--- problem A/pizzaforces.cpp
+++ problem A/pizzaforces.cpp
@@ -1,19 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
+using dll = double long;
 int main(){
     ll t;
     cin>>t;
     while(t--){
-        ll n;
+        dll n;
         cin>>n;
-        pair<ll, ll> s = {6, 15};
-        pair<ll, ll> m = {8, 20};
-        pair<ll, ll> l = {10, 25};
-        pair<ll, ll> sm = {14, 35};
-        pair<ll, ll> sl = {16, 40};
-        pair<ll, ll> ml = {18, 45};
-        pair<ll, ll> sml = {24, 60};
         
+        // WRONG APPROACH
+        // pair<dll, dll> s = {6, 15};
+        // pair<dll, dll> m = {8, 20};
+        // pair<dll, dll> l = {10, 25};
+        // pair<dll, dll> sm = {14, 35};
+        // pair<dll, dll> sl = {16, 40};
+        // pair<dll, dll> ml = {18, 45};
+        // pair<dll, dll> sml = {24, 60};
+        // ll s1 = ceil(n/s.first)*s.second;
+        // ll m1 = ceil(n/m.first)*m.second;
+        // ll l1 = ceil(n/l.first)*l.second;
+        // ll sm1 = ceil(n/sm.first)*sm.second;
+        // ll sl1 = ceil(n/sl.first)*sl.second;
+        // ll ml1 = ceil(n/ml.first)*ml.second;
+        // ll sml1 = ceil(n/sml.first)*sml.second;
+        // cout<<min({s1, m1, l1, sm1, sl1, ml1, sml1})<<endl;
+
     }
 }
