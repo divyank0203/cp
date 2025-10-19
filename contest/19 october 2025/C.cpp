e29908596bd62ae217da066c09ad5f842f279e35
divyank0203 <divyankh444@gmail.com>
2025-10-19 22:10:47 +0530

    contest

--- contest/19 october 2025/C.cpp
+++ contest/19 october 2025/C.cpp
@@ -1,38 +1,115 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// using ll = long long;
+// using vll = vector<ll>;
+// int main(){
+//     ll t;
+//     cin>>t;
+//     while(t--){
+//         ll n;
+//         cin>>n;
+//         vll a(n);
+//         for(ll i=0; i<n; i++){
+//             cin>>a[i];
+//         }
+//         vll b(n);
+//         for(ll i=0; i<n; i++){
+//             cin>>b[i];
+//         }
+//         ll ans=2;
+//         ll even=0;
+//         for(ll i=0; i<n; i++){
+//             if(a[i]%2==0){
+//                 even++;
+//             }
+//         }
+//         if(even>=2){
+//             cout<<0<<endl;
+//         }
+//         else if(even==1){
+//             cout<<1<<endl;
+//         }
+//         else{
+            
+//         }
+        
+        
+//     }
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
-using vll = vector<ll>;
+unordered_set<ll> factor_set(ll n) {
+    unordered_set<ll> st;
+    for (ll i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            st.insert(i);
+            while (n % i == 0){
+                n /= i;
+            }
+        }
+    }
+    if (n > 1) {
+        st.insert(n);
+    }
+    return st;
+}
+
 int main(){
     ll t;
     cin>>t;
     while(t--){
         ll n;
         cin>>n;
-        vll a(n);
+        vector<ll> a(n);
+        vector<ll> b(n);
         for(ll i=0; i<n; i++){
             cin>>a[i];
         }
-        vll b(n);
         for(ll i=0; i<n; i++){
             cin>>b[i];
         }
-        ll ans=2;
-        ll even=0;
+        unordered_map<ll, ll> primes;
+        ll ans=0;
+        bool zero=false;
         for(ll i=0; i<n; i++){
-            if(a[i]%2==0){
-                even++;
+            auto s = factor_set(a[i]);
+            for(auto p:s){
+                primes[p]++;
+                if(primes[p]>=2){
+                    zero=true;
+                    break;
+                }
+            }
+            if(zero){
+                break;
             }
         }
-        if(even>=2){
+        if(zero){
             cout<<0<<endl;
         }
-        else if(even==1){
+        else{
+        bool one=false;
+        for(ll i=0; i<n; i++){
+            auto s = factor_set(a[i]+1);
+            for(auto p:s){
+                if(primes.find(p)!=primes.end()&&primes[p]>0){
+                    one=true;
+                    break;
+                }
+            }
+            if(one){
+                break;
+            }
+        }
+        if(one){
             cout<<1<<endl;
         }
         else{
-            
+            cout<<2<<endl;
         }
-        
-        
+        }
+
     }
 }
--- primefactors.cpp
+++ primefactors.cpp
@@ -1,20 +1,74 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
-// Returns the set of distinct prime factors of n
 unordered_set<ll> factor_set(ll n) {
     unordered_set<ll> st;
     for (ll i = 2; i * i <= n; i++) {
         if (n % i == 0) {
             st.insert(i);
-            while (n % i == 0)
+            while (n % i == 0){
                 n /= i;
+            }
         }
     }
-    if (n > 1) st.insert(n);
+    if (n > 1) {
+        st.insert(n);
+    }
     return st;
 }
 
 int main(){
-    
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        vector<ll> a(n);
+        vector<ll> b(n);
+        for(ll i=0; i<n; i++){
+            cin>>a[i];
+        }
+        for(ll i=0; i<n; i++){
+            cin>>b[i];
+        }
+        unordered_map<ll, ll> primes;
+        ll ans=0;
+        bool zero=false;
+        for(ll i=0; i<n; i++){
+            auto s = factor_set(a[i]);
+            for(auto p:s){
+                primes[p]++;
+                if(primes[p]>=2){
+                    zero=true;
+                    break;
+                }
+            }
+            if(zero){
+                break;
+            }
+        }
+        if(zero){
+            cout<<0<<endl;
+            continue;
+        }
+        bool one=false;
+        for(ll i=0; i<n; i++){
+            auto s = factor_set(a[i]+1);
+            for(auto p:s){
+                if(primes.find(p)!=primes.end()&&primes[p]>0){
+                    one=true;
+                    break;
+                }
+            }
+            if(one){
+                break;
+            }
+        }
+        if(one){
+            cout<<1<<endl;
+        }
+        else{
+            cout<<2<<endl;
+        }
+    }
 }
