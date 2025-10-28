b64b3abd42833c622f2e41b5a5e5873d787f324b
divyank0203 <divyankh444@gmail.com>
2025-10-28 14:23:02 +0530

    contest

---
+++ contest/28 october 2025/E.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+#define pb push_back;
+
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        
+    }
+}
