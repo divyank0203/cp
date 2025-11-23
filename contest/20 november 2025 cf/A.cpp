ab178f6b466bd4c22ec4d34d3282a9c2d4ab4cdf
divyank0203 <divyankh444@gmail.com>
2025-11-23 16:58:47 +0530

    contest

--- contest/20 november 2025 cf/A.cpp
+++ contest/20 november 2025 cf/A.cpp
@@ -5,6 +5,25 @@ int main(){
     ll t;
     cin>>t;
     while(t--){
-        
+        int n;
+        cin>>n;
+        vector<ll> a(n);
+        unordered_map<ll,ll> mp;
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+            mp[a[i]]++;
+        }
+        ll ans=0;
+        for(auto x: mp){
+            if(x.first!=x.second){
+                if(x.second>x.first){
+                    ans+=x.second - x.first;
+                }
+                else{
+                    ans+=x.second;
+                }
+            }
+        }
+        cout<<ans<<endl;
     }
 }
--- contest/20 november 2025 cf/B.cpp
+++ contest/20 november 2025 cf/B.cpp
@@ -5,6 +5,34 @@ int main(){
     ll t;
     cin>>t;
     while(t--){
-        
+        ll n, x, y;
+        cin>>n>>x>>y;
+        string s;
+        cin>>s;
+        ll cnt0=0;
+        ll cnt1=0;
+        for(ll i=0; i<n; i++){
+            if(s[i]=='4'){
+                cnt0++;
+            }
+            else{
+                cnt1++;
+            }
+        }
+        ll xa=abs(x);
+        ll ya=abs(y);
+        ll diagonal=min((ll)cnt1, min(xa, ya));
+        cnt1-=diagonal;
+        // x-=diagonal;
+        // y-=diagonal;
+        xa-=diagonal;
+        ya-=diagonal;
+        ll rem=(xa+ya);
+        if(rem<=(cnt0+cnt1)){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
     }
 }
