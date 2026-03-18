7e669bd2223320c3ff468bdf63cf8d59e5b69ac3
divyank0203 <divyankh444@gmail.com>
2026-03-18 22:08:46 +0530

    contest

--- contest/11 feb 2026 cc/hh/A.cpp
+++ contest/11 feb 2026 cc/hh/A.cpp
@@ -1,18 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
-using ll = long long ;
+using ll = long long;
 int main(){
     ll t;
     cin>>t;
     while(t--){
-        ll x, y, n;
-        cin>>x>>y>>n;
-        ll left=(x-(y*n));
-        if(left<=0){
-            cout<<0<<endl;
+        ll n;
+        cin >> n;
+        vector<ll> a(n);
+        for (ll i = 0; i < n; i++) cin >> a[i];
+        
+        vector<ll> order(n);
+        iota(order.begin(), order.end(), 0);
+        sort(order.begin(), order.end(), [&](ll x, ll y) {
+            return a[x] > a[y];
+        });
+        
+        set<ll> seen;
+        seen.insert(-1);
+        seen.insert(n);
+        
+        long long ans = 0;
+        
+        for (ll i : order) {
+            auto it = seen.insert(i).first;
+            
+            auto lit = prev(it);
+            auto rit = next(it);
+            
+            ll l = *lit;
+            ll r = *rit;
+            
+            ll pl = (l == -1) ? -1 : *prev(lit);
+            ll pr = (r == n) ?  n : *next(rit);
+            
+            if (l != -1) {
+                ans += (long long)a[i] * (l - pl) * (r - i);
+            }
+            if (r != n) {
+                ans += (long long)a[i] * (i - l) * (pr - r);
+            }
         }
-        else{
-            cout<<left<<endl;
-        }
+        
+        cout << ans << endl;
+
+
     }
 }
